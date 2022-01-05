c2ef5982af6ebf657ac22564f8aa3aae8d1ba86e
Chandelure Wang <chandelurewang@gmail.com>
2022-01-05 22:02:37 +0800

    Create base framework (#3)

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# luci-app-simple-clash
+# Luci App Simple Clash
+
+## Description
+
+LuCI support for Clash. Configuration of clash based on https://github.com/chandelures/openwrt-clash to build your tunnel on Openwrt.
+
+## Installation
+
+### Manual Install
+
+1. Download Openwrt Source Code or SDK as the basic enviroment to build the package.
+
+```shell
+$ git clone https://github.com/openwrt/openwrt
+$ cd openwrt
+
+# or 
+
+$ wget https://downloads.openwrt.org/path/to/openwrt-sdk_*.tar.xz
+$ tar -Jxvf openwrt-sdk_*.tar.xz
+$ cd openwrt-sdk_*
+```
+
+2. Prepare build environment
+
+```shell
+$ ./scripts/feeds update -a
+$ ./scripts/feeds install -a
+
+$ git clone https://github.com/chandelures/openwrt-clash package/openwrt-clash
+$ git clone https://github.com/chandelures/luci-app-simple-clash package/luci-app-simple-clash
+```
+
+3. Choose luci-app-simple-clash as a module or built-in module
+
+```shell
+$ make menuconfig
+
+...
+
+LuCI  --->
+    Applications  --->
+        <M> luci-app-simple-clash
+
+...
+
+```
+
+4. Build packages
+```shell
+$ make package/luci-app-simple-clash/{clean,compile} V=s
+```
---
+++ htdocs/luci-static/resources/view/clash/overview.js
@@ -0,0 +1,428 @@
+"use strict";
+"require ui";
+"require form";
+"require view";
+"require uci";
+"require rpc";
+"require fs";
+
+return view.extend({
+  callGetServiceStatus: rpc.declare({
+    object: "luci.clash",
+    method: "get_service_status",
+    expect: {},
+  }),
+  load: function () {
+    return Promise.all([this.callGetServiceStatus(), uci.load("clash")]);
+  },
+  render: function (data) {
+    var status = data[0];
+    var m, s, o;
+
+    m = new form.Map(
+      "clash",
+      _("Simple Clash"),
+      _(
+        'LuCI support for clash to translate traffic. More infomation is available at the <a href="https://github.com/Dreamacro/clash/wiki">official ducument<a/>'
+      )
+    );
+
+    s = m.section(form.NamedSection, "global", null, _("Infomations"));
+
+    o = s.option(form.DummyValue, null, "Status");
+    o.cfgvalue = function () {
+      return status["enabled"] ? "Running" : "Not Running";
+    };
+
+    o = s.option(form.DummyValue, null, "Version");
+    o.cfgvalue = function () {
+      return status["version"];
+    };
+
+    if (status["dashboard"]) {
+      o = s.option(form.DummyValue, null, "Web Interface");
+      o.cfgvalue = function () {
+        return E(
+          "botton",
+          {
+            class: "cbi-button cbi-button-apply",
+            click: function () {
+              var path = "clash-dashboard";
+              var host = window.location.host;
+              var protocol = window.location.protocol;
+              window.open(
+                "%s//%s/%s?hostname=%s".format(protocol, host, path, host)
+              );
+            },
+          },
+          _("Dashboard")
+        );
+      };
+    }
+
+    s = m.section(form.NamedSection, "global", "clash", _("Settings"));
+    s.tab("general", _("Basic Options"));
+    s.tab("addition", _("Addtional Options"));
+    s.tab("dns", _("DNS Settings"));
+
+    o = s.taboption(
+      "general",
+      form.Flag,
+      "enabled",
+      _("Enabled"),
+      _("Enable the clash service.")
+    );
+    o.rmempty = false;
+
+    o = s.taboption(
+      "general",
+      form.Flag,
+      "tproxy_enabled",
+      _("TProxy Enabled"),
+      _("Enable the transparent proxy.")
+    );
+    o.rmempty = false;
+
+    o = s.taboption(
+      "general",
+      form.Value,
+      "tproxy_port",
+      _("TProxy Port"),
+      _("The port of transparent proxy server.")
+    );
+    o.depends("tproxy_enabled", "1");
+    o.datatype = "port";
+    o.rmempty = false;
+    o.default = 7893;
+
+    o = s.taboption(
+      "general",
+      form.Value,
+      "current_profile",
+      _("Profile"),
+      _("List of avaliable configurations for clash.")
+    );
+    for (var v of L.uci.sections("clash", "profile")) {
+      o.value(v[".name"], v[".name"]);
+    }
+    o.rmempty = false;
+
+    o = s.taboption(
+      "general",
+      form.Value,
+      "mode",
+      _("Mode"),
+      _("Clash router mode.")
+    );
+    o.value("direct", "Direct");
+    o.value("rule", "Rule");
+    o.value("global", "Global");
+    o.rmempty = false;
+    o.default = "Rule";
+
+    o = s.taboption(
+      "general",
+      form.Value,
+      "http_port",
+      _("Http Port"),
+      _("The port of http proxy server. Set 0 to close http proxy.")
+    );
+    o.datatype = "port";
+    o.rmempty = false;
+    o.default = 7890;
+
+    o = s.taboption(
+      "general",
+      form.Value,
+      "socks_port",
+      _("Socks Port"),
+      _("The port of socks proxy server. Set 0 to close socks proxy.")
+    );
+    o.datatype = "port";
+    o.rmempty = false;
+    o.default = 7891;
+
+    o = s.taboption(
+      "general",
+      form.Value,
+      "mixed_port",
+      _("Mixed Port"),
+      _("The port of http and socks proxy server. Set 0 to close mixed proxy.")
+    );
+    o.datatype = "port";
+    o.rmempty = false;
+    o.default = 0;
+
+    o = s.taboption(
+      "addition",
+      form.Value,
+      "prog",
+      _("Custom Path"),
+      _("The path of clash to execute.")
+    );
+    o.datatype = "string";
+    o.rmempty = false;
+    o.default = "/usr/bin/clash";
+
+    o = s.taboption(
+      "addition",
+      form.Flag,
+      "allow_lan",
+      _("Allow Lan"),
+      _("Allow connections to the local server.")
+    );
+    o.rmempty = false;
+
+    o = s.taboption(
+      "addition",
+      form.Value,
+      "bind_addr",
+      _("Bind Address"),
+      _(
+        "IP addresses be allowed to create connections. By default, this value is 0.0.0.0."
+      )
+    );
+    o.depends("allow_lan", "1");
+    o.datatype = "ipaddr";
+    o.rmempty = false;
+    o.default = "0.0.0.0";
+
+    o = s.taboption(
+      "addition",
+      form.Flag,
+      "udp_tproxy_enabled",
+      _("UDP Tproxy"),
+      _("Enable UDP transparent proxy.")
+    );
+    o.rmempty = false;
+
+    o = s.taboption(
+      "addition",
+      form.Value,
+      "log_level",
+      _("Log Level"),
+      _("Clash by default prints log.")
+    );
+    o.value("silent", "Silent");
+    o.value("debug", "Debug");
+    o.value("error", "Error");
+    o.value("warning", "Warning");
+    o.value("info", "Info");
+    o.rmempty = false;
+
+    o = s.taboption(
+      "addition",
+      form.Value,
+      "api_host",
+      _("Api Address"),
+      _("The host of Clash RESTful API.")
+    );
+    o.datatype = "ipaddr";
+    o.rmempty = false;
+
+    o = s.taboption(
+      "addition",
+      form.Value,
+      "api_port",
+      _("Api Port"),
+      _("The port of Clash RESTful API.")
+    );
+    o.datatype = "port";
+    o.rmempty = false;
+
+    o = s.taboption(
+      "dns",
+      form.Value,
+      "dns_host",
+      _("DNS Host"),
+      _("The host of Clash built-in DNS server.")
+    );
+    o.datatype = "ipaddr";
+    o.rmempty = false;
+
+    o = s.taboption(
+      "dns",
+      form.Value,
+      "dns_port",
+      _("DNS Port"),
+      _("The port of Clash build-in DNS server.")
+    );
+    o.datatype = "port";
+    o.rmempty = false;
+
+    o = s.taboption(
+      "dns",
+      form.Value,
+      "dns_mode",
+      _("DNS Mode"),
+      _(
+        "DNS server enhanced mode. Redir-host mode will response true results, Fake-ip mode will response fake results quickly but may bring some issues."
+      )
+    );
+    o.value("redir-host", "redir-host");
+    o.value("fake-ip", "fake-ip");
+    o.rmempty = false;
+
+    o = s.taboption(
+      "dns",
+      form.DynamicList,
+      "default_nameserver",
+      _("Default Nameservers"),
+      _(
+        "Nameservers are used to resolve the DNS nameserver hostnames. Only support UDP."
+      )
+    );
+    o.datatype = "ipaddr";
+    o.rmempty = false;
+
+    o = s.taboption(
+      "dns",
+      form.DynamicList,
+      "nameserver",
+      _("Nameservers"),
+      _(
+        "Nameservers are used to resolve all DNS query. Support UDP, TCP, DOT, DOH."
+      )
+    );
+    o.datatype = "string";
+    o.rmempty = false;
+
+    o = s.taboption(
+      "dns",
+      form.DynamicList,
+      "fallback",
+      _("Fallbacks"),
+      _(
+        "Nameservers are used to resolve the DNS query when GEOIP contry is not 'CN'. Support UDP, TCP, DOT, DOH."
+      )
+    );
+    o.depends("dns_mode", "redir-host");
+    o.datatype = "string";
+    o.rmempty = false;
+
+    s = m.section(form.GridSection, "profile", _("Profiles"));
+    s.sortable = true;
+    s.addremove = true;
+    s.anonymous = true;
+    s.addbtntitle = _("Add new profiles...");
+    s.modaltitle = function (section_id) {
+      return _("Clash Profiles - %s".format(section_id));
+    };
+
+    o = s.option(form.DummyValue, "_cfg_name", _("Name"));
+    o.modalonly = false;
+    o.textvalue = function (section_id) {
+      return section_id;
+    };
+
+    o = s.option(form.Value, "type", _("Type"));
+    o.value("Static", "Static");
+    o.rmempty = false;
+
+    o = s.option(form.TextValue, null, _("Content"));
+    o.modalonly = true;
+    o.monospace = true;
+    o.rows = 25;
+    o.load = function (section_id) {
+      return fs
+        .read("/etc/clash/profiles/%s.yaml".format(section_id), "")
+        .then(function (value) {
+          return value;
+        })
+        .catch(function (e) {
+          return "";
+        });
+    };
+    o.write = function (section_id, formvalue) {
+      return fs
+        .write("/etc/clash/profiles/%s.yaml".format(section_id), formvalue)
+        .then(function () {
+          s.textvalue = formvalue;
+          ui.addNotification(null, E("p", _("Changes have been saved.")));
+        })
+        .catch(function (e) {
+          ui.addNotification(
+            null,
+            E("p", _("Unable to save changes: %s").format(e.nessage))
+          );
+        });
+    };
+
+    s.handleCreateProfile = function (m, name, type, ev) {
+      var section_id = name.isValid("_new_") ? name.formvalue("_new_") : null;
+      var type_value = type.isValid("_new_") ? type.formvalue("_new_") : null;
+
+      if (section_id == null || type_value == "") return;
+
+      return m
+        .save(function () {
+          section_id = uci.add("clash", "profile", section_id);
+          uci.set("clash", section_id, "type", type_value);
+        })
+        .then(L.bind(s.renderMoreOptionsModal, s, section_id));
+    };
+
+    s.handleAdd = function (ev) {
+      var m2, s2, name, type;
+
+      m2 = new form.Map("clash");
+      s2 = m2.section(form.NamedSection, "_new_");
+      s2.render = function () {
+        return Promise.all([{}, this.renderUCISection("_new_")]).then(
+          this.renderContents.bind(this)
+        );
+      };
+
+      name = s2.option(form.Value, "name", _("Name"));
+      name.rmempty = false;
+      name.datatype = "uciname";
+      name.validate = function (section_id, value) {
+        if (uci.get("clash", value) != null)
+          return _("The profile name is already used");
+        return true;
+      };
+
+      type = s2.option(form.Value, "type", _("Type"));
+      type.rmempty = false;
+      type.value("Static", "Static");
+      type.default = "Static";
+
+      m2.render().then(
+        L.bind(function (nodes) {
+          ui.showModal(
+            _("Add new profile..."),
+            [
+              nodes,
+              E("div", { class: "right" }, [
+                E("button", { class: "btn", click: ui.hideModal }, _("Cancel")),
+                " ",
+                E(
+                  "button",
+                  {
+                    class: "cbi-button cbi-button-positive important",
+                    click: ui.createHandlerFn(
+                      this,
+                      "handleCreateProfile",
+                      m,
+                      name,
+                      type
+                    ),
+                  },
+                  _("Create profile")
+                ),
+              ]),
+            ],
+            "cbi-modal"
+          );
+          nodes
+            .querySelector(
+              '[id="%s"] input[type="text"]'.format(name.cbid("_new_"))
+            )
+            .focus();
+        }, this)
+      );
+    };
+
+    return m.render();
+  },
+});
